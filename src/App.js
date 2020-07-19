1c3a8027f179e1a0e08c17b55ff04eabb34df0ef
raquel-gonzo <gonzalez.rachel.c@gmail.com>
2020-07-18 20:04:16 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import PersonCard from './components/PersonCard';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PersonCard lastName="Gonzalez" firstName="Rachel" age={24} hairColor="brown"/>
+      <PersonCard lastName="Earls" firstName="Tyler" age={27} hairColor="brown"/>
+      <PersonCard lastName="Sanrio" firstName="Gudetama" age={7} hairColor="no hair!"/>
+      <PersonCard lastName="Smith" firstName="Maria" age={62} hairColor="pink"/>
     </div>
   );
 }
